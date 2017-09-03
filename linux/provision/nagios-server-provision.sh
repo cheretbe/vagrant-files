28497a3352567cac78523746de85c22369a66167
Alexey Orlov <a.a.orlov@gmail.com>
2017-09-03 19:30:56 +0200

    Add common provision script for Nagios server

---
+++ linux/provision/nagios-server-provision.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -euo pipefail
+
+if [ -e "/etc/centos-release" ]; then
+  distro = "centos"
+elif [ -e "/etc/debian_version" ]; then
+  distro = "debian"
+  if grep -q Ubuntu /etc/os-release; then distro = "ubuntu"; fi
+else
+  >&2 echo "Unsupported Linux distribution"
+  exit 1
+fi
+
+echo ${distro}
