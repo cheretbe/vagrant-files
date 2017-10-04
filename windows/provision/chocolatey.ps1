a2697c7401e4d8b2314eae9111547e99f99e6a0c
cheretbe <a.a.orlov@gmail.com>
2017-10-04 15:54:25 +0200

    Finish Chocolatey installation script

--- windows/provision/chocolatey.ps1
+++ windows/provision/chocolatey.ps1
@@ -1,7 +1,14 @@
-# Set-ExecutionPolicy Bypass; iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/cheretbe/vagrant-files/develop/windows/provision/chocolatey.ps1'))
+# Set-ExecutionPolicy Bypass -Scope Process; iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/cheretbe/vagrant-files/develop/windows/provision/chocolatey.ps1'))
 
 Set-StrictMode -Version Latest
 $ErrorActionPreference = [System.Management.Automation.ActionPreference]::Stop
 $Host.PrivateData.VerboseForegroundColor = [ConsoleColor]::DarkCyan
 
-Write-Host "There you go" -ForegroundColor Cyan
+Set-ExecutionPolicy Bypass -Scope Process
+
+if ((Get-Command "choco.exe" -ErrorAction SilentlyContinue) -eq $NULL) {
+  Write-Output "Chocolatey is not installed. Installing"
+  Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+} else {
+  Write-Output "Chocolatey is already installed"
+} #if
