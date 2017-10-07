6bd8649be292ad3486dd7d1707b19737bcbcdbd5
cheretbe <a.a.orlov@gmail.com>
2017-10-07 21:39:32 +0200

    Add repo checkout

--- windows/provision/windows-config-builder.bat
+++ windows/provision/windows-config-builder.bat
@@ -4,3 +4,4 @@ ECHO Starting powershell...
 powershell.exe -NoProfile -NonInteractive -ExecutionPolicy Bypass -File "%~dp0%~n0.ps1"
 
 PATH %PATH%;%ProgramData%\chocolatey\bin;%ProgramFiles%\Git\cmd
+cd "%USERPROFILE%\projects\windows-config"
--- windows/provision/windows-config-builder.ps1
+++ windows/provision/windows-config-builder.ps1
@@ -7,3 +7,11 @@ $script:scriptDir = Split-Path -Parent $MyInvocation.MyCommand.Path
 . (Join-Path -Path $script:scriptDir -ChildPath "chocolatey.ps1")
 . (Join-Path -Path $script:scriptDir -ChildPath "git.ps1")
 
+New-Item -ItemType Directory -Path ($env:USERPROFILE + "\projects") -Force | Out-Null 
+Set-Location -Path ($env:USERPROFILE + "\projects")
+RunConsoleCommand -command "git.exe" -parameters @("clone", "--recursive", "https://git.beercaps.ru/orlov/windows-config.git")
+
+Set-Location -Path "./windows-config"
+RunConsoleCommand -command "git.exe" -parameters @("checkout", "develop")
+RunConsoleCommand -command "git.exe" -parameters @("status")
+# RunConsoleCommand -command "git.exe" -parameters @("checkout", "-t", "origin/develop")
