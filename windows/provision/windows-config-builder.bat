6ea500134400afc15590b61ed34b84d7bea97746
cheretbe <a.a.orlov@gmail.com>
2017-12-18 16:02:10 +0200

    Add online mode windows-config builder VM provisioner

--- windows/provision/windows-config-builder.bat
+++ windows/provision/windows-config-builder.bat
@@ -1,7 +1,7 @@
 @ECHO OFF
 
 ECHO Starting powershell...
-powershell.exe -NoProfile -NonInteractive -ExecutionPolicy Bypass -File "%~dp0%~n0.ps1"
+powershell.exe -NoProfile -NonInteractive -ExecutionPolicy Bypass -File "%~dp0%~n0.ps1" -offline
 
 PATH %PATH%;%ProgramData%\chocolatey\bin;%ProgramFiles%\Git\cmd
 cd "%USERPROFILE%\projects\windows-config"
--- windows/provision/windows-config-builder.ps1
+++ windows/provision/windows-config-builder.ps1
@@ -1,15 +1,34 @@
+[CmdletBinding()]
+param(
+  [switch]$offline,
+  [switch]$develop
+)
+
 Set-StrictMode -Version Latest
 $ErrorActionPreference = [System.Management.Automation.ActionPreference]::Stop
 $Host.PrivateData.VerboseForegroundColor = [ConsoleColor]::DarkCyan
 
 $script:scriptDir = Split-Path -Parent $MyInvocation.MyCommand.Path
 
-. (Join-Path -Path $script:scriptDir -ChildPath "chocolatey.ps1")
-. (Join-Path -Path $script:scriptDir -ChildPath "git.ps1")
+if ($offline.IsPresent) {
+  . (Join-Path -Path $script:scriptDir -ChildPath "chocolatey.ps1")
+  . (Join-Path -Path $script:scriptDir -ChildPath "git.ps1")
+} else {
+  $repoPath = "https://raw.githubusercontent.com/cheretbe/vagrant-files"
+  $gitBranch = if ($develop.IsPresent) { "develop" } else { "master" }
+  Invoke-Expression ((New-Object System.Net.WebClient).DownloadString(
+    ("{}/{}/windows/provision/chocolatey.ps1" -f $repoPath, $gitBranch))
+  Invoke-Expression ((New-Object System.Net.WebClient).DownloadString(
+    ("{}/{}/windows/provision/git.ps1" -f $repoPath, $gitBranch))
+} #if
 
 New-Item -ItemType Directory -Path ($env:USERPROFILE + "\projects") -Force | Out-Null 
 Set-Location -Path ($env:USERPROFILE + "\projects")
-RunConsoleCommand -command "git.exe" -parameters @("clone", "--recursive", "https://git.beercaps.ru/orlov/windows-config.git")
+if (-not(Test-Path -Path "./windows-config")) {
+  RunConsoleCommand -command "git.exe" -parameters @(
+    "clone", "--recursive", "https://git.beercaps.ru/orlov/windows-config.git"
+  )
+} #if
 
 Set-Location -Path "./windows-config"
 RunConsoleCommand -command "git.exe" -parameters @("checkout", "develop")
