10c9bae81084f2dc2c824a5ff50022e47e49d9e6
cheretbe <a.a.orlov@gmail.com>
2017-12-21 22:06:49 +0200

    Remove batch wrapper

--- windows/provision/windows-config-builder.bat
+++
@@ -1,7 +0,0 @@
-@ECHO OFF
-
-ECHO Starting powershell...
-powershell.exe -NoProfile -NonInteractive -ExecutionPolicy Bypass -File "%~dp0%~n0.ps1" -offline %*
-
-PATH %PATH%;%ProgramData%\chocolatey\bin;%ProgramFiles%\Git\cmd
-cd "%USERPROFILE%\projects\windows-config"
