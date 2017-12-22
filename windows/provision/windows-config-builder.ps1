415b7deea4939a686b3cfa4af462f26766f7f418
cheretbe <a.a.orlov@gmail.com>
2017-12-22 15:25:57 +0200

    Move script loading to a separate function to simplify debugging

--- windows/provision/windows-config-builder.ps1
+++ windows/provision/windows-config-builder.ps1
@@ -24,21 +24,51 @@ $Host.PrivateData.VerboseForegroundColor = [ConsoleColor]::DarkCyan
 
 $script:scriptDir = Split-Path -Parent $MyInvocation.MyCommand.Path
 
-if ($localTest.IsPresent) {
-  Write-Verbose ("Local testing mode")
-  Invoke-Expression ((Get-Content -Path (${Env:USERPROFILE} + '/provision/chocolatey.ps1') | Out-String))
-  Invoke-Expression ((Get-Content -Path (${Env:USERPROFILE} + '/provision/git.ps1') | Out-String))
-} elseif (Test-Path -Path (Join-Path -Path $script:scriptDir -ChildPath "chocolatey.ps1")) {
-  . (Join-Path -Path $script:scriptDir -ChildPath "chocolatey.ps1")
-  . (Join-Path -Path $script:scriptDir -ChildPath "git.ps1")
-} else {
-  $repoPath = "https://raw.githubusercontent.com/cheretbe/vagrant-files"
-  $gitBranch = if ($develop) { "develop" } else { "master" }
-  Invoke-Expression ((New-Object System.Net.WebClient).DownloadString(
-    ("{0}/{1}/windows/provision/chocolatey.ps1" -f $repoPath, $gitBranch)))
-  Invoke-Expression ((New-Object System.Net.WebClient).DownloadString(
-    ("{0}/{1}/windows/provision/git.ps1" -f $repoPath, $gitBranch)))
-} #if
+function RunScript {
+[CmdletBinding()]
+param(
+  [string]$scriptName
+)
+  if ($localTest.IsPresent)
+    { Write-Verbose ("Local testing mode") }
+
+  $localFilePath = Join-Path -Path $script:scriptDir -ChildPath $scriptName
+  if (Test-Path -Path $localFilePath) {
+    Write-Host ("Executing '{0}'" -f $localFilePath)
+    . $localFilePath
+  } else {
+    if ($localTest.IsPresent) {
+      $localFilePath = Join-Path -Path ${Env:USERPROFILE} -ChildPath ('provision/' + $scriptName)
+      Write-Host ("Invoking '{0}'" -f $localFilePath)
+      $scriptContents = Get-Content -Path $localFilePath | Out-String
+    } else {
+      $gitBranch = if ($develop) { "develop" } else { "master" }
+      $scriptURL = ("https://raw.githubusercontent.com/cheretbe/vagrant-files/{0}/windows/provision/{1}" -f $gitBranch, $scriptName)
+      Write-Host ("Invoking '{0}'" -f $scriptURL)
+      $scriptContents = (New-Object System.Net.WebClient).DownloadString($localFilePath)
+    } #if
+    Invoke-Expression $scriptContents
+  } #if
+}
+
+RunScript -scriptName "chocolatey.ps1"
+RunScript -scriptName "git.ps1"
+
+# if ($localTest.IsPresent) {
+#   Write-Verbose ("Local testing mode")
+#   Invoke-Expression ((Get-Content -Path (${Env:USERPROFILE} + '/provision/chocolatey.ps1') | Out-String))
+#   Invoke-Expression ((Get-Content -Path (${Env:USERPROFILE} + '/provision/git.ps1') | Out-String))
+# } elseif (Test-Path -Path (Join-Path -Path $script:scriptDir -ChildPath "chocolatey.ps1")) {
+#   . (Join-Path -Path $script:scriptDir -ChildPath "chocolatey.ps1")
+#   . (Join-Path -Path $script:scriptDir -ChildPath "git.ps1")
+# } else {
+#   $repoPath = "https://raw.githubusercontent.com/cheretbe/vagrant-files"
+#   $gitBranch = if ($develop) { "develop" } else { "master" }
+#   Invoke-Expression ((New-Object System.Net.WebClient).DownloadString(
+#     ("{0}/{1}/windows/provision/chocolatey.ps1" -f $repoPath, $gitBranch)))
+#   Invoke-Expression ((New-Object System.Net.WebClient).DownloadString(
+#     ("{0}/{1}/windows/provision/git.ps1" -f $repoPath, $gitBranch)))
+# } #if
 
 New-Item -ItemType Directory -Path ($env:USERPROFILE + "\projects") -Force | Out-Null 
 Set-Location -Path ($env:USERPROFILE + "\projects")
