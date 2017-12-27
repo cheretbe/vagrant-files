88f2810f89df34e0af385faa115f8c9932817293
cheretbe <a.a.orlov@gmail.com>
2017-12-27 10:19:55 +0200

    Fix online/local invocation logic

--- windows/provision/common.psm1
+++ windows/provision/common.psm1
@@ -3,26 +3,44 @@ $ErrorActionPreference = [System.Management.Automation.ActionPreference]::Stop
 $Host.PrivateData.VerboseForegroundColor = [ConsoleColor]::DarkCyan
 
 $script:scriptDir = Split-Path -Parent $MyInvocation.MyCommand.Path
-$script:provisionLocalTestMode = $FALSE
-# Write-Host $MyInvocation.MyCommand.Path -ForegroundColor Cyan
-# Write-Host ("localTest: {0}" -f $localTest.IsPresent)
+$global:provisionLocalTestMode = $FALSE
 
 function Set-VagrantProvisionLocalTestMode {
 [CmdletBinding()]
 param(
   [bool]$testMode
 )
-  $script:provisionLocalTestMode = $testMode
+  $global:provisionLocalTestMode = $testMode
 }
 
 function Invoke-VagrantProvisionScript {
 [CmdletBinding()]
 param(
   [string]$scriptName,
-  [hashtable]$scriptParams
+  [string]$gitBranch = "master",
+  [hashtable]$scriptParams = @{}
 )
-  Write-Host ("provisionLocalTestMode: {0}" -f $script:provisionLocalTestMode) -ForegroundColor Cyan
-  Write-Verbose ("scriptName: {0}, scriptParams: {1}" -f $scriptName, ($scriptParams | Out-String))
+  # if ($global:provisionLocalTestMode)
+  #   { Write-Host ("Local testing mode") -ForegroundColor Cyan }
+
+  $localFilePath = Join-Path -Path $script:scriptDir -ChildPath $scriptName
+  if ((Test-Path -Path $localFilePath) -and (-not ($global:provisionLocalTestMode))) {
+    Write-Host ("Dot-sourcing '{0}'" -f $localFilePath)
+    . $localFilePath @scriptParams
+  } else {
+    if ($global:provisionLocalTestMode) {
+      $localSource = Join-Path -Path ${Env:USERPROFILE} -ChildPath ('provision/' + $scriptName)
+      Write-Host ("Copying '{0}'" -f $localSource)
+      $scriptContents = Get-Content -Path $localSource | Out-String
+    } else {
+      $scriptURL = ("https://raw.githubusercontent.com/cheretbe/vagrant-files/{0}/windows/provision/{1}" -f $gitBranch, $scriptName)
+      Write-Host ("Downloading '{0}'" -f $scriptURL)
+      $scriptContents = (New-Object System.Net.WebClient).DownloadString($scriptURL)
+    } #if
+    $scriptContents | Out-File -FilePath $localFilePath -Force
+    Write-Host ("Dot-sourcing '{0}'" -f $localFilePath)
+    . $localFilePath @scriptParams
+  } #if
 }
 
 Export-ModuleMember -Function Set-VagrantProvisionLocalTestMode
