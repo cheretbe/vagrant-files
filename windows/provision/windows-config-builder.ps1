8581d8db445f20b99e7175b2d5c105be81ca62df
cheretbe <a.a.orlov@gmail.com>
2018-02-06 16:51:12 +0200

    Add automatic credentials saving from environment variables

--- windows/provision/windows-config-builder.ps1
+++ windows/provision/windows-config-builder.ps1
@@ -14,12 +14,26 @@ $Host.PrivateData.ErrorBackgroundColor = $Host.UI.RawUI.BackgroundColor
 
 $script:scriptDir = Split-Path -Parent $MyInvocation.MyCommand.Path
 Import-Module (Join-Path -Path $script:scriptDir -ChildPath "common.psm1")
+$script:gitlabHost = "git.beercaps.ru"
 
 Invoke-VagrantProvisionScript -scriptName "chocolatey.ps1"
 Invoke-VagrantProvisionScript -scriptName "git.ps1"
 Invoke-VagrantProvisionScript -scriptName "set-env-variables.ps1"
 
 Invoke-VagrantProvisionConsoleCommand -command "choco.exe" -parameters @("install", "Nuget.CommandLine", "-y", "--no-progress")
+
+$credentialsFile = (Join-Path -Path $env:USERPROFILE -ChildPath ".git-credentials")
+if ((Test-Path "Env:\AO_DEFAULT_GITLAB_USER") -and (Test-Path "Env:\AO_DEFAULT_GITLAB_PASSWORD")) {
+  if ($NULL -eq (Get-Content $credentialsFile -ErrorAction SilentlyContinue | Where-Object { $_.Contains($script:gitlabHost) })) {
+    Write-Host ("Adding credentials to '{0}'" -f $credentialsFile)
+    # Add-Content -Path $credentialsFile ("https://{0}:{1}@{2}`n" -f `
+    #   $Env:AO_DEFAULT_GITLAB_USER, $Env:AO_DEFAULT_GITLAB_PASSWORD, $script:gitlabHost)
+    [System.IO.File]::AppendAllText($credentialsFile, ("https://{0}:{1}@{2}`n" -f `
+      $Env:AO_DEFAULT_GITLAB_USER, $Env:AO_DEFAULT_GITLAB_PASSWORD, $script:gitlabHost))
+  } else {
+    Write-Host ("'{0}' already contains credentials for {1}" -f $credentialsFile, $script:gitlabHost)
+  } #if
+} #if
 
 New-Item -ItemType Directory -Path ($env:USERPROFILE + "\projects") -Force | Out-Null 
 Set-Location -Path ($env:USERPROFILE + "\projects")
@@ -32,6 +46,25 @@ if (-not(Test-Path -Path "./windows-config")) {
 Set-Location -Path "./windows-config"
 Invoke-VagrantProvisionConsoleCommand -command "git.exe" -parameters @("checkout", "develop")
 Invoke-VagrantProvisionConsoleCommand -command "git.exe" -parameters @("status")
+
+if (Test-Path "Env:\AO_DEFAULT_GITLAB_USER") {
+  Write-Host ("Setting 'user.name' parameter to '{0}'" -f $Env:AO_DEFAULT_GITLAB_USER)
+  Invoke-VagrantProvisionConsoleCommand -command "git.exe" `
+    -parameters @("config", "user.name", $Env:AO_DEFAULT_GITLAB_USER)
+} #if
+
+if (Test-Path "Env:\AO_DEFAULT_GITLAB_EMAIL") {
+  Write-Host ("Setting 'user.email' parameter to '{0}'" -f $Env:AO_DEFAULT_GITLAB_EMAIL)
+  Invoke-VagrantProvisionConsoleCommand -command "git.exe" `
+    -parameters @("config", "user.email", $Env:AO_DEFAULT_GITLAB_EMAIL)
+} #if
+
+if (Test-Path -Path $credentialsFile) {
+  Invoke-VagrantProvisionConsoleCommand -command "git.exe" `
+    -parameters @("config", "--global", "credential.helper", "store")
+  Invoke-VagrantProvisionConsoleCommand -command "git.exe" `
+    -parameters @("config", "--system", "credential.helper", "store")
+}
 
 if (-not(Get-Module "AWSPowerShell" -ListAvailable)) {
   Write-Host "Installing NuGet package provider"
