7bd2009d2a7de8ec766ab9bdc512ef3001785523
cheretbe <a.a.orlov@gmail.com>
2018-03-18 22:17:59 +0200

    Finish script upload on Windows

--- mikrotik/provision/common.rb
+++ mikrotik/provision/common.rb
@@ -1,12 +1,14 @@
-# echo.|net use \\172.28.128.3\backup /user:vagrant /persistent:no
-# net use \\172.28.128.3\backup /d
+def upload_file(file_path, target_name, script_name)
+  # info ("scp -P #{@machine.ssh_info[:port]} -i #{@machine.ssh_info[:private_key_path][0]} " +
+           # "-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null " +
+           # "mt_client_provision.rsc #{@machine.ssh_info[:username]}@#{@machine.ssh_info[:host]}:/")
 
-def upload_file(machine, file_path)
-  info "There you go"
-  info ("scp -P #{@machine.ssh_info[:port]} -i #{@machine.ssh_info[:private_key_path][0]} " +
-           "-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null " +
-           "mt_client_provision.rsc #{@machine.ssh_info[:username]}@#{@machine.ssh_info[:host]}:/")
   if Vagrant::Util::Platform.windows? then
-    info "++++"
+    run ("powershell.exe -ExecutionPolicy Bypass -File ..\\provision\\smb_upload_file.ps1 -filePath " +
+      file_path + " -targetName " + target_name)
   end
+
+  run ("vagrant ssh -- /system script add name=" + script_name + "
+    source=[/file get " + target_name + " contents]")
+  run ("vagrant ssh -- :delay 5; /file remove " + target_name)
 end
