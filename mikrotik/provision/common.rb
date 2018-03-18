43674ac0d970ba4d7f5447749c525c5f6306c0f7
cheretbe <a.a.orlov@gmail.com>
2018-03-18 22:45:04 +0200

    Update upload script (enable SMB on Windows and delete existing script before uploading)

--- mikrotik/provision/common.rb
+++ mikrotik/provision/common.rb
@@ -3,7 +3,11 @@ def upload_file(file_path, target_name, script_name)
            # "-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null " +
            # "mt_client_provision.rsc #{@machine.ssh_info[:username]}@#{@machine.ssh_info[:host]}:/")
 
+  run ("vagrant ssh -- :if ([:len [/system script find name=\"" + script_name +
+    "\"]] != 0) do={ /system script remove " + script_name +" }")
+
   if Vagrant::Util::Platform.windows? then
+    run ("vagrant ssh -- /system script run enable_smb")
     run ("powershell.exe -ExecutionPolicy Bypass -File ..\\provision\\smb_upload_file.ps1 -filePath " +
       file_path + " -targetName " + target_name)
   end
