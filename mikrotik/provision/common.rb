9a2d733f1c7e0227439c508407c7c43ac76fb8e5
cheretbe <a.a.orlov@gmail.com>
2018-03-19 11:02:56 +0200

    Uncomment scp call on non-Windows systems

--- mikrotik/provision/common.rb
+++ mikrotik/provision/common.rb
@@ -1,8 +1,4 @@
 def upload_file(file_path, target_name, script_name)
-  # info ("scp -P #{@machine.ssh_info[:port]} -i #{@machine.ssh_info[:private_key_path][0]} " +
-           # "-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null " +
-           # "mt_client_provision.rsc #{@machine.ssh_info[:username]}@#{@machine.ssh_info[:host]}:/")
-
   run ("vagrant ssh -- :if ([:len [/system script find name=\"" + script_name +
     "\"]] != 0) do={ /system script remove " + script_name +" }")
 
@@ -10,6 +6,10 @@ def upload_file(file_path, target_name, script_name)
     run ("vagrant ssh -- /system script run enable_smb")
     run ("powershell.exe -ExecutionPolicy Bypass -File ..\\provision\\smb_upload_file.ps1 -filePath " +
       file_path + " -targetName " + target_name)
+  else
+    run ("scp -P #{@machine.ssh_info[:port]} -i #{@machine.ssh_info[:private_key_path][0]} " +
+      "-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null " +
+      "mt_client_provision.rsc #{@machine.ssh_info[:username]}@#{@machine.ssh_info[:host]}:/")
   end
 
   run ("vagrant ssh -- /system script add name=" + script_name + "
