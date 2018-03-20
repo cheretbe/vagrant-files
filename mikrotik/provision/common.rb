7d03d2e1f08e1f85187046e25c15315b548a5fbe
cheretbe <a.a.orlov@gmail.com>
2018-03-20 11:00:11 +0200

    Add comment on SSH LogLevel option

--- mikrotik/provision/common.rb
+++ mikrotik/provision/common.rb
@@ -7,6 +7,7 @@ def upload_file(file_path, target_name, script_name)
     run ("powershell.exe -ExecutionPolicy Bypass -File ..\\provision\\smb_upload_file.ps1 -filePath " +
       file_path + " -targetName " + target_name + " -machineName #{@machine.name}")
   else
+    # "-o", "LogLevel=FATAL" ?
     run ("scp -P #{@machine.ssh_info[:port]} -i #{@machine.ssh_info[:private_key_path][0]} " +
       "-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null " +
       "mt_client_provision.rsc #{@machine.ssh_info[:username]}@#{@machine.ssh_info[:host]}:/")
