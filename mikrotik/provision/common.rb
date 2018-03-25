a7e8e1a0285a11b27020e1aad44b2426894f96bb
cheretbe <a.a.orlov@gmail.com>
2018-03-25 18:52:11 +0200

    Add scp option to log only fatal errors (to suppress ssh host key warning)

--- mikrotik/provision/common.rb
+++ mikrotik/provision/common.rb
@@ -7,9 +7,8 @@ def upload_file(file_path, target_name, script_name)
     run ("powershell.exe -ExecutionPolicy Bypass -File ..\\provision\\smb_upload_file.ps1 -filePath " +
       file_path + " -targetName " + target_name + " -machineName #{@machine.name}")
   else
-    # "-o", "LogLevel=FATAL" ?
     run ("scp -P #{@machine.ssh_info[:port]} -i #{@machine.ssh_info[:private_key_path][0]} " +
-      "-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null " +
+      "-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o LogLevel=FATAL " +
       "#{file_path} #{@machine.ssh_info[:username]}@#{@machine.ssh_info[:host]}:/")
   end
 
