9902b96f9595f8218a49822fb64e518a96029ef8
cheretbe <a.a.orlov@gmail.com>
2018-03-28 10:52:49 +0200

    Fix network config during provision

--- mikrotik/2isps-pppoe/remote_server_provision.sh
+++ mikrotik/2isps-pppoe/remote_server_provision.sh
@@ -2,24 +2,31 @@
 
 set -euo pipefail
 
-if ! grep -q "route add -net 192.168.51.0/24" /etc/rc.local; then
-  echo "Updating '/etc/rc.local' (static route to 192.168.51.0/24)"
-  sed -i "s!^exit 0!/sbin/route add -net 192.168.51.0/24 gw 172.24.0.1!" /etc/rc.local
-  echo "exit 0" >> /etc/rc.local
+if [ -z ${1+x} ]; then
+  echo "Mandatory parameter is missing - 1: 'vagrant-inter_isp-intnet' NIC MAC address"
+  exit 1
 fi
 
-if ! grep -q "route add -net 192.168.52.0/24" /etc/rc.local; then
-  echo "Updating '/etc/rc.local' (static route to 192.168.52.0/24)"
-  sed -i "s!^exit 0!/sbin/route add -net 192.168.52.0/24 gw 172.24.0.2!" /etc/rc.local
-  echo "exit 0" >> /etc/rc.local
-fi
+for iface in $(ls /sys/class/net/); do
+  mac_address=$(cat "/sys/class/net/${iface}/address")
+  # Convert to upper case before comparing (^^)
+  if [ "${mac_address^^}" == "${1}" ]; then
+    inter_isp_iface=${iface}
+  fi
+done
 
-if ! grep -q "192.168.51.0/24" <<< "$(ip route)"; then
-  echo "Adding static route to 192.168.51.0/24"
-  route add -net 192.168.51.0/24 gw 172.24.0.1
-fi
+echo "'vagrant-inter_isp-intnet' interface name: ${inter_isp_iface}"
 
-if ! grep -q "192.168.52.0/24" <<< "$(ip route)"; then
-  echo "Adding static route to 192.168.52.0/24"
-  route add -net 192.168.52.0/24 gw 172.24.0.2
+if [ ! -e "/etc/network/interfaces.d/${inter_isp_iface}.cfg" ]; then
+	echo "Creating '/etc/network/interfaces.d/${inter_isp_iface}.cfg'"
+	cat <<-EOF > /etc/network/interfaces.d/${inter_isp_iface}.cfg
+		auto ${inter_isp_iface}
+		iface ${inter_isp_iface} inet static
+		address 172.24.0.3
+		netmask 255.255.255.0
+		up /sbin/route add -net 192.168.51.0/24 gw 172.24.0.1 || true
+		up /sbin/route add -net 192.168.52.0/24 gw 172.24.0.2 || true
+	EOF
+	echo "Restarting network"
+	service networking restart
 fi
