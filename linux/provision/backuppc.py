ab6a447f0e8dc76da806aff2ad4c8086f234991b
cheretbe <a.a.orlov@gmail.com>
2018-11-25 16:43:48 +0200

    Update BackupPC provision script (not finished)

--- linux/provision/backuppc.py
+++ linux/provision/backuppc.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
 
 import os
+import sys
 import apt
 import subprocess
 import requests
 import shutil
+import distutils.version
+import pwd
 
 needed_packages = ["apache2", "apache2-utils", "libapache2-mod-perl2",
     "smbclient", "postfix", "libapache2-mod-scgi", "libarchive-zip-perl",
@@ -24,7 +27,7 @@ def download_file(url):
     return local_filename
 
 
-# run("apt-get -q update")
+run("apt-get -q update")
 packages_to_install = []
 apt_cache = apt.Cache()
 for needed_package in needed_packages:
@@ -36,6 +39,42 @@ if len(packages_to_install) != 0:
     print("Installing packages: " + ", ".join(packages_to_install))
     # DEBIAN_FRONTEND variable is set to avoid postfix package showing configuration dialog
     run("DEBIAN_FRONTEND=noninteractive apt-get install -y -q " + " ".join(packages_to_install))
+
+installed_version = None
+upgrade_mode = False
+# We are looking for a line like this:
+# '# Version 4.2.1, released 7 May 2018.'
+if os.path.isfile("/usr/local/BackupPC/bin/BackupPC"):
+    with open("/usr/local/BackupPC/bin/BackupPC", "r") as f:
+        for line in f:
+            if line.startswith("# Version "):
+                installed_version = line[10:].split(",")[0]
+                break
+if installed_version:
+    print("Currently isntalled version is: " + installed_version)
+    latest_version = requests.get("https://api.github.com/repos/backuppc/backuppc/releases/latest").json()["tag_name"]
+    print("Latest version is: " + latest_version)
+    if distutils.version.LooseVersion(latest_version) > distutils.version.LooseVersion(installed_version):
+        print("Upgrading {} ==> {}".format(installed_version, latest_version))
+        upgrade_mode = True
+    else:
+        print("BackupPC does not need an upgrade. Exiting")
+        sys.exit(0)
+
+if not upgrade_mode:
+    print("Installing BackupPC")
+    user_exists = False
+    try:
+        pwd.getpwnam("backuppc")
+        user_exists = True
+    except KeyError:
+        pass
+    if user_exists:
+        raise Exception("User `backuppc' already exists. Exiting")
+    run("adduser --system --home /var/lib/backuppc --group --disabled-password --shell /bin/false backuppc")
+
+print("there you go")
+sys.exit(0)
 
 src_dir = os.path.expanduser("~/source")
 os.makedirs(src_dir, exist_ok=True)
