7e4eb38c24986d1de987378c32200320172ebc7d
cheretbe <a.a.orlov@gmail.com>
2018-11-26 16:51:30 +0200

    Finish BackupPC provision script

--- linux/provision/backuppc.py
+++ linux/provision/backuppc.py
@@ -17,6 +17,8 @@ parser.add_argument("-n", "--hostname", dest="hostname", default="backuppc",
     help="Host name (default: backuppc)")
 parser.add_argument("-а", "--force-version", dest="force_version", default=None,
     help="Set specific version to install", metavar="VERSION")
+parser.add_argument('-b', '--batch', dest='batch_mode', action='store_true',
+    default=False, help='Run in batch mode without any prompts')
 
 options = parser.parse_args()
 
@@ -48,8 +50,11 @@ show_postfix_warning = not apt_cache["postfix"].is_installed
 
 if len(packages_to_install) != 0:
     print("Installing packages: " + ", ".join(packages_to_install))
-    # DEBIAN_FRONTEND variable is set to avoid postfix package showing configuration dialog
-    run("DEBIAN_FRONTEND=noninteractive apt-get install -y -q " + " ".join(packages_to_install))
+    if options.batch_mode:
+        # DEBIAN_FRONTEND variable is set to avoid postfix package showing configuration dialog
+        run("DEBIAN_FRONTEND=noninteractive apt-get install -y -q " + " ".join(packages_to_install))
+    else:
+        run("apt-get install -y " + " ".join(packages_to_install))
 
 installed_version = None
 upgrade_mode = False
@@ -73,7 +78,12 @@ if installed_version:
         sys.exit(0)
 
 if upgrade_mode:
-    run("service backuppc stop")
+
+    print("'backuppc' service status:")
+    # 'systemctl is-active' echoes current status and sets exit code to 0 if it is 'active'
+    if subprocess.run("systemctl is-active backuppc.service", shell=True).returncode == 0:
+        print("Stopping 'backuppc' service")
+        run("service backuppc stop")
 else:
     print("Installing BackupPC")
 
@@ -106,9 +116,6 @@ else:
     run("chmod 600 /var/lib/backuppc/.ssh/id_rsa")
     run("chmod 644 /var/lib/backuppc/.ssh/id_rsa.pub")
     run("chown -R backuppc:backuppc /var/lib/backuppc/.ssh")
-
-# print("there you go")
-# sys.exit(0)
 
 src_dir = os.path.expanduser("~/source")
 os.makedirs(src_dir, exist_ok=True)
@@ -147,6 +154,66 @@ else:
     run("./configure.pl --batch --cgi-dir /var/www/cgi-bin/BackupPC "
         "--data-dir /var/lib/backuppc --hostname {} --html-dir /var/www/html/BackupPC "
         "--html-dir-url /BackupPC --install-dir /usr/local/BackupPC".format(options.hostname))
+    run("cp httpd/BackupPC.conf /etc/apache2/conf-available/backuppc.conf")
+
+    # Allows to connect to web UI from anywhere, not only from 127.0.0.1 by removing the following lines:
+    # order deny,allow
+    # deny from all
+    # allow from 127.0.0.1
+    with open("/etc/apache2/conf-available/backuppc.conf", "r+") as f:
+        old_lines = f.readlines()
+        f.seek(0)
+        for line in old_lines:
+            if line not in ("order deny,allow\n", "deny from all\n", "allow from 127.0.0.1\n"):
+                f.write(line)
+        f.truncate()
+
+    run("cp /etc/apache2/envvars /etc/apache2/envvars.bak")
+    # Note that changing the apache user and group (next two commands) could cause other services
+    # provided by apache to fail. There are alternatives if you don't want to change the apache
+    # user: use SCGI or a setuid BackupPC_Admin script - see the docs.
+    run('sed -i "s/export APACHE_RUN_USER=www-data/export APACHE_RUN_USER=backuppc/" /etc/apache2/envvars')
+    run('sed -i "s/export APACHE_RUN_GROUP=www-data/export APACHE_RUN_GROUP=backuppc/" /etc/apache2/envvars')
+
+    run("cp /var/www/html/index.html /var/www/html/index.html.bak")
+    with open("/var/www/html/index.html", "w") as f:
+        f.write('<html><head><meta http-equiv="refresh" content="0; url=/BackupPC_Admin"></head></html>\n')
+
+    run("a2enconf backuppc")
+    run("a2enmod cgid")
+    run("service apache2 restart")
+
+    run("cp systemd/backuppc.service /etc/systemd/system")
+    run('sed -i "s/#Group=backuppc/Group=backuppc/" /etc/systemd/system/backuppc.service')
+    run("systemctl daemon-reload")
+    run("systemctl enable backuppc.service")
+
+    run("chmod u-s /var/www/cgi-bin/BackupPC/BackupPC_Admin")
+    run("touch /etc/BackupPC/BackupPC.users")
+    run("cp /etc/BackupPC/config.pl /etc/BackupPC/config.pl.bak")
+    # Replace
+    #    $Conf{CgiAdminUserGroup} = '';
+    #    $Conf{CgiAdminUsers}     = '';
+    # with
+    #    $Conf{CgiAdminUserGroup} = 'backuppc';
+    #    $Conf{CgiAdminUsers}     = 'backuppc';
+    with open("/etc/BackupPC/config.pl", "r+") as f:
+        old_lines = f.readlines()
+        f.seek(0)
+        for line in old_lines:
+            # if line not in ("order deny,allow\n", "deny from all\n", "allow from 127.0.0.1\n"):
+            if line == "$Conf{CgiAdminUserGroup} = '';\n":
+                line = "$Conf{CgiAdminUserGroup} = 'backuppc';\n"
+            if line == "$Conf{CgiAdminUsers}     = '';\n":
+                line = "$Conf{CgiAdminUsers}     = 'backuppc';\n"
+            f.write(line)
+        f.truncate()
+
+    run("chown -R backuppc:backuppc /etc/BackupPC")
+
+    run("htpasswd /etc/BackupPC/BackupPC.users backuppc")
+
+run("service backuppc start")
 
 if show_postfix_warning:
     print("\033[93m\n[!] Don't forget to configure MTA with 'dpkg-reconfigure postfix --priority low' \033[0m")
