9c5c8989b24033a3fd6002c91c1fe123ac13afb3
cheretbe <a.a.orlov@gmail.com>
2018-12-10 19:12:53 +0200

    Fix ssh command escaping under Windows

--- host-scripts/common.rb
+++ host-scripts/common.rb
@@ -61,14 +61,14 @@ end
 def upload_mikrotik_script(machine, source_file_path, target_script_name)
   puts "Uploading script '#{target_script_name}' from file '#{source_file_path}'"
 
-  system ("vagrant ssh #{machine.name} -- ':if ([:len [/system script find " +
+  system("vagrant", "ssh", "#{machine.name}", "--", ":if ([:len [/system script find " +
     "name=\"#{target_script_name}\"]] != 0) do={ :put \"Target script already exists. Removing\"; " +
-    "/system script remove #{target_script_name} }'")
+    "/system script remove #{target_script_name} }")
 
   machine.communicate.tap do |comm|
     comm.upload(source_file_path, "temp_script_2del.rsc")
   end
-  system ("vagrant ssh #{machine.name} -- /system script add name=" + target_script_name +
-    " source=[/file get temp_script_2del.rsc contents]")
-  system ("vagrant ssh #{machine.name} -- ':delay 5; /file remove temp_script_2del.rsc'")
+  system("vagrant", "ssh", "#{machine.name}", "--", "/system script add name=" +
+    target_script_name + " source=[/file get temp_script_2del.rsc contents]")
+  system("vagrant", "ssh", "#{machine.name}", "--", ":delay 5; /file remove temp_script_2del.rsc")
 end
