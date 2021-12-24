2af4481a4f55baf525e05bf0a5c2d2df4ecf808e
cheretbe <a.a.orlov@gmail.com>
2021-12-24 12:19:40 +0200

    testlabs/ansible: use insecure vagrant key for Ansible authentication

--- testlabs/ansible/provision/setup_host.py
+++ testlabs/ansible/provision/setup_host.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python3
 
-with open ("/vagrant/provision/id_rsa.pub", "r") as fh:
-    ansible_pub_key = fh.read().strip()
+# ssh-keygen -yf ~/.vagrant.d/insecure_private_key
+ansible_pub_key = (
+    "ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEA6NF8iallvQVp22WDkTkyrtvp9eWW6A8YVr+kz"
+    "4TjGYe7gHzIw+niNltGEFHzD8+v1I2YJ6oXevct1YeS0o9HZyN1Q9qgCgzUFtdOKLv6Iedplq"
+    "oPkcmF0aYet2PkEDo3MlTBckFXPITAMzF8dJSIFo9D8HfdOV0IAdx4O7PtixWKn5y2hMNG0zQ"
+    "PyUecp4pzC6kivAIhyfHilFR61RGL+GPXQ2MWZWFYbAGjyiYJnAmCP3NOTd0jMZEnDkbUvxhM"
+    "mBYSdETk1rRgm+R4LOzFUGaHqHDLKLX+FIPKcF96hrucXzcWyLbIbEgE98OHlnVYCzRdK8jlq"
+    "m8tehUc9c9WhQ=="
+)
 
 with open("/home/vagrant/.ssh/authorized_keys", "r+") as fh:
     for line in fh:
